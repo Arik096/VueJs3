83324c5fc9280e6689157dc6b4540cde5c5fc8dd
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-01-15 20:12:12 +0600

    Update app.js

--- part_4/app.js
+++ part_4/app.js
@@ -1,12 +1,7 @@
 var app = Vue.createApp({
     data(){
         return{
-            info: {
-                name: "Arik Md Isthiaque",
-                git: "https://github.com/",
-                fb: "arikmdisthiaque",
-                html: '<p>get connected to <a target="_blank" href="info.git">GitHub</a></p>',
-            }
+            msg : "welcome Arik"
         };
     }
 });
