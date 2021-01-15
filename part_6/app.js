b648d5d683a21f34d5d9654f81ed4ae4560319c9
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-01-15 21:24:10 +0600

    Update app.js

--- part_6/app.js
+++ part_6/app.js
@@ -1,8 +1,22 @@
 var app = Vue.createApp({
     data(){
         return{
-            msg : "welcome Arik"
+            count: 0
         };
+    },
+    methods: {
+        getCurrentTime(){
+            let ct = new Date();
+            return ct;
+        },
+        plus(){
+            this.count++;
+        },
+        minus(){
+            if(this.count > 0){
+                this.count--;
+            }
+        }
     }
 });
 
