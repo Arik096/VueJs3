f174af4d0d730735102a95bb1dcf77963d863923
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-01-15 21:16:39 +0600

    Update app.js

--- part_4/app.js
+++ part_4/app.js
@@ -1,7 +1,14 @@
 var app = Vue.createApp({
     data(){
         return{
-            msg : "welcome Arik"
+            msg : "welcome Arik",
+            info: {
+                name: "Arik Md Isthiaque",
+                age: "25",
+                job: "software engineer",
+                link: "https://vuejs.org/",
+                html: "<h1>Haha</h1>"
+            }
         };
     }
 });
