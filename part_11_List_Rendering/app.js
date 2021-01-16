526f51d8df9697c388a466f95b36c8f0c61cd7e7
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-01-16 19:56:48 +0600

    Update app.js

--- part_11_List_Rendering/app.js
+++ part_11_List_Rendering/app.js
@@ -1,11 +1,22 @@
 var app = Vue.createApp({
   data() {
     return {
-        bgColor: "green",
-        tColor: "red"
+      skills: [
+        { name: "html", exp: 3 },
+        { name: "css", exp: 3 },
+        { name: "js", exp: 1 }
+      ],
+      newSkill: {}
     };
   },
   methods: {
+    addSkill() {
+      this.skills.push({name: this.newSkill, exp: 0});
+      this.newSkill = {};
+    },
+    revomeListItem(i) {
+      this.skills.splice(i, 1);
+    },
   },
 });
 
