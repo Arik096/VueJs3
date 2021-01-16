74e4d2e528cae0a15ff529ed5b525bbc538e80a0
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-01-16 15:47:53 +0600

    Update app.js

--- part_9_Class_binding/app.js
+++ part_9_Class_binding/app.js
@@ -1,26 +1,14 @@
 var app = Vue.createApp({
-    data(){
-        return{
-            number: "",
-            result: "",
-            msg: ""
-        };
-    },
-    methods: {
-        getDouble(){
-            this.result = this.number * 2;
-        },
-        getSquare(){
-            this.result = this.number ** 2;
-        },
-        add10(){
-            this.result = this.number + 10;
-        },
-        getReset(){
-            this.result = "";
-            this.number = "";
-        }
-    }
+  data() {
+    return {
+        activeClass: "red"
+    };
+  },
+  methods: {
+      setClass(color){
+          this.activeClass = color;
+      }
+  },
 });
 
 app.mount("#app");
