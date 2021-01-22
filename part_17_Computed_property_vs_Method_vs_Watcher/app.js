03a11e231b943c7f73dda436caff1dd7a5eb5411
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-01-22 21:40:53 +0600

    Update app.js

--- part_17_Computed_property_vs_Method_vs_Watcher/app.js
+++ part_17_Computed_property_vs_Method_vs_Watcher/app.js
@@ -1,12 +1,48 @@
 var app = Vue.createApp({
   data() {
     return {
-        bgColor: "green",
-        tColor: "red"
+      mobile: "",
+      name: {
+        firstName: "",
+        lasrName: ""
+      }
     };
+
   },
+
+  computed: {
+  },
+
   methods: {
   },
+
+  watch: {
+    mobile(newValue, oldValue){
+      console.log(newValue);
+      
+      if(isNaN(newValue)){
+        alert("Enter Valid Mobile Number");
+        this.mobile = oldValue;
+      }
+
+      if(newValue.length ==  11)
+      {
+        alert("An OTP has been sent")
+      }
+    },
+
+    // "name.firstName": function(newValue, oldValue){
+    //   console.log(newValue, oldValue);
+    // },
+
+    name: {
+      deep: true,
+      handler: function(newValue, oldValue){
+       console.log(newValue, oldValue); 
+      }
+    }
+  },
+
 });
 
 app.mount("#app");
