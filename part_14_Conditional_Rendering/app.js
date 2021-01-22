49abe3bb9924adbe70f5203c19e20a52ebeaa7aa
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-01-22 19:27:21 +0600

    Update app.js

--- part_14_Conditional_Rendering/app.js
+++ part_14_Conditional_Rendering/app.js
@@ -1,19 +1,26 @@
 var app = Vue.createApp({
   data() {
     return {
-      aboutMe: {
-        name: "arik",
-        job: "coder",
-        skill: "php"
-      },
+      show: false,
       skills: [
-        { name: "html", exp: 5 },
-        { name: "css", exp: 7 },
-        { name: "sass", exp: 8 }
-      ]
+        { name: "js", exp: 12 },
+        { name: "css", exp: 22 },
+        { name: "html", exp: 32 },
+        { name: "sass", exp: 42 },
+        { name: "c", exp: 25 },
+        { name: "c++", exp: 23 },
+        { name: "java", exp: 22 },
+        { name: "py", exp: 23 },
+        { name: "hk", exp: 22 },
+        { name: "tf", exp: 21 },
+      ],
     };
   },
-  methods: {}
+  methods: {
+    toggle(){
+      this.show = !this.show;
+    }
+  },
 });
 
 app.mount("#app");
