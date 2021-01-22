bd1eca8f81136020b059530246b803c764f502fd
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-01-22 20:29:29 +0600

    Update app.js

--- part_15_Computed_Properties/app.js
+++ part_15_Computed_Properties/app.js
@@ -1,11 +1,36 @@
 var app = Vue.createApp({
   data() {
     return {
-        bgColor: "green",
-        tColor: "red"
+      skills: [
+        { name: "Html", Exp: 2 },
+        { name: "css", Exp: 3 },
+        { name: "sass", Exp: 1 },
+        { name: "js", Exp: 15 },
+        { name: "node", Exp: 12 },
+        { name: "vue", Exp: 3 },
+        { name: "python", Exp: 15 },
+        { name: "java", Exp: 3 },
+      ],
     };
   },
+  computed: {
+    totalCount1() {
+      return this.skills.length;
+    },
+    expSkills(){
+      let es = this.skills.filter(item=> {
+        return item.Exp >=10;
+      })
+      return es;
+    }
+  },
   methods: {
+    removeFromList(i) {
+      this.skills.splice(i, 1);
+    },
+    totalCount() {
+      return this.skills.length;
+    },
   },
 });
 
