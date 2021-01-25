00a8b59e6a774ee96376efc07bc53b68ead9ea44
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-01-25 12:56:43 +0600

    Update app.js

--- part_19_Ticket_Booking_App_Coding/by_me/app.js
+++ part_19_Ticket_Booking_App_Coding/by_me/app.js
@@ -1,6 +1,16 @@
 var app = Vue.createApp({
     data(){
         return {
+            SeatList: {
+                A1: "booked", A2: "", A3: "", A4: "",
+                B1: "", B2: "", B3: "", B4: "",
+                C1: "", C2: "", C3: "", C4: "",
+                D1: "", D2: "", D3: "", D4: "",
+                E1: "", E2: "", E3: "", E4: "",
+                E1: "", E2: "", E3: "", E4: ""
+                
+            },
+            seatBGColor: "",
         };
     },
 
@@ -9,6 +19,17 @@ var app = Vue.createApp({
     computed: {},
 
     watch: {},
+
+    mounted(){
+        for(key in this.SeatList){
+            if(this.SeatList[key] == "booked"){
+                this.seatBGColor = "#868383";
+            }
+            else{
+                this.seatBGColor = "#FFFFFF";
+            }
+        }
+    }
 
 });
 
