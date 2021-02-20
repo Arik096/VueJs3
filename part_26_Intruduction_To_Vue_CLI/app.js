1914bbdc28d42a391dde3a0a1774310ba53a31da
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-02-20 14:13:07 +0600

    Create app.js

---
+++ part_26_Intruduction_To_Vue_CLI/app.js
@@ -0,0 +1,44 @@
+var app1 = Vue.createApp({
+  data() {
+    return {
+      input: "",
+    };
+  },
+
+  computed: {},
+
+  methods: {
+    clear() {
+      this.input = "";
+    },
+  },
+
+  watch: {},
+
+  template: `
+      <br />
+      <input type="text" v-model="input" />
+      <button @click="clear()">clear</button>
+      <p>name : {{input}}</p>
+  `,
+});
+
+app1.mount("#app1");
+
+var app2 = Vue.createApp({
+  data() {
+    return {
+      msg: "welcome",
+    };
+  },
+
+  template: "<h2>{{msg}}</h2>",
+
+  computed: {},
+
+  methods: {},
+
+  watch: {},
+});
+
+app2.mount("#app2");
